65600f4217d75a012c845ad8ff32dc0b42a80bc9
Carlos E. Salazar <ce.salazar@gmail.com>
2018-07-20 03:16:59 +0200

    Add list of all the events with a component

---
+++ .vuepress/components/EventsList.vue
@@ -0,0 +1,38 @@
+<!--
+  Created by: Carlos E. Salazar <ce.salazar@gmail.com>
+  License: MIT
+-->
+
+<template>
+  <div class="events--container">
+    <div v-for="event in events" v-if="event.frontmatter.title">
+      <!-- Event's name -->
+      <h2>
+        <a :href="event.path">{{ event.frontmatter.title }}</a>
+      </h2>
+
+      <!-- Event's description -->
+      <p>{{ event.frontmatter.description }}</p>
+
+      <!-- Date and time -->
+      <p>{{ event.frontmatter.date }} @ {{ event.frontmatter.time }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    events () {
+      return this.$site.pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+.events--container
+  div
+    margin 2em 0 4em
+    border-bottom 1px solid darken(#E9EBEE, 5%)
+</style>
