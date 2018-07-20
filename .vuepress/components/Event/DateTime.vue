ce1b055d1f66e762d2488ac03559512da8eb3951
Carlos E. Salazar <ce.salazar@gmail.com>
2018-07-20 16:17:52 +0200

    Add DateTime component

---
+++ .vuepress/components/Event/DateTime.vue
@@ -0,0 +1,26 @@
+<!--
+  Created by: Carlos E. Salazar <ce.salazar@gmail.com>
+  License: MIT
+-->
+
+<template>
+  <p>
+    {{ datetime }}
+  </p>
+</template>
+
+<script>
+export default {
+  props: ['date', 'time'],
+  computed: {
+    datetime () {
+      let date = this.date || this.$page.frontmatter.date
+      date = new Date(date)
+
+      let time = this.time || this.$page.frontmatter.time
+
+      return `${date.toDateString()} @ ${time}`
+    }
+  }
+}
+</script>
