625f8697ecc0f82ef8f53e562c4b993d2de2bfec
Carlos E. Salazar <ce.salazar@gmail.com>
2018-07-20 15:23:04 +0200

    Refactor TicketsLink component
    It now uses the ExternalLink component, and shows the price before the CTA
    button.

--- .vuepress/components/Event/TicketsLink.vue
+++ .vuepress/components/Event/TicketsLink.vue
@@ -5,30 +5,34 @@
 
 <template>
   <div class="ticket--container">
-    <!-- Link to ticket sale -->
-    <span v-if="$page.frontmatter.tickets" class="button">
-      <a :href="$page.frontmatter.tickets" target="_blank" rel="noopener noreferrer">
-        Tickets
-        <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" x="0px" y="0px" viewBox="0 0 100 100" width="15" height="15" class="icon outbound"><path fill="currentColor" d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"></path><polygon fill="currentColor" points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"></polygon>
-        </svg>
-      </a>
-    </span>
-
     <!-- Ticket price -->
     <span class="ticket--price" v-if="$page.frontmatter.price > 0">
       <b>{{ "Cost: " + $page.frontmatter.price + "€" }}</b> 
     </span>
+
+    <!-- Link to ticket sale -->
+    <span v-if="$page.frontmatter.tickets" class="button">
+      <ExternalLink
+        :url="$page.frontmatter.tickets"
+        caption="Tickets"
+        indicator="true"
+      />
+    </span>
   </div>
 </template>
+
+<script>
+import ExternalLink from '../Utils/ExternalLink'
+
+export default {
+  components: { ExternalLink }
+}
+</script>
 
 <style lang="stylus">
 .ticket--container
   margin-top 1.5em
 
 .ticket--price
-  margin-left 1em
-
-hr
-  margin-top 2em
-  margin-bottom 1em
+  margin-right 1em
 </style>
