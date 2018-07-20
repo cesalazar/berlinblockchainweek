075b67d7632093432375341f26f34b5659fe2825
Carlos E. Salazar <ce.salazar@gmail.com>
2018-07-20 18:02:15 +0200

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+vuepress build
+
+# navigate into the build output directory
+cd .vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:cesalazar/berlinblockchainweek.git master:gh-pages
+
+cd -
