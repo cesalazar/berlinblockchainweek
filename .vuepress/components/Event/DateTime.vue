67efdee7b780174143d8684260dcfb7f5b7dc814
Carlos E. Salazar <ce.salazar@gmail.com>
2018-07-23 00:57:43 +0200

    Refactor DateTime component
    This now displays the full name for the day and the month.

--- .vuepress/components/Event/DateTime.vue
+++ .vuepress/components/Event/DateTime.vue
@@ -12,14 +12,42 @@
 <script>
 export default {
   props: ['date', 'time'],
+  data: () => ({
+    dayNames: [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+    ],
+    monthNames: [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ]
+  }),
   computed: {
     datetime () {
       let date = this.date || this.$page.frontmatter.date
       date = new Date(date)
 
+      let day = this.dayNames[date.getDay()]
+      let month = this.monthNames[date.getMonth()]
+
       let time = this.time || this.$page.frontmatter.time
 
-      return `${date.toDateString()} @ ${time}`
+      return `${day}, ${date.getDate()} ${month} @ ${time}`
     }
   }
 }
