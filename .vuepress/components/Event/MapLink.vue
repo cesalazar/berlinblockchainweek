d75a2bac20b21ce2714b46b6e51dc55332fb3e94
Carlos E. Salazar <ce.salazar@gmail.com>
2018-07-29 18:42:32 +0200

    Add support for more than one venue. Close #25
    This solution is prone to errors as it requires an event to have the same
    number of venues and addresses.

--- .vuepress/components/Event/MapLink.vue
+++ .vuepress/components/Event/MapLink.vue
@@ -5,17 +5,19 @@
 -->
 
 <template>
-  <p>
-    <span v-if="$page.frontmatter.venue">
-      {{ $page.frontmatter.venue }}<br/>
-    </span>
+  <div>
+    <p v-for="venue in venues">
+      <span v-if="venue.name">
+        {{ venue.name }}<br/>
+      </span>
 
-    <!-- TODO: encode the address -->
-    <ExternalLink
-      :url="'https://www.google.com/maps/place/' + $page.frontmatter.address"
-      :caption="$page.frontmatter.address"
-    />
-  </p>
+      <ExternalLink
+        :url="'https://www.google.com/maps/place/' + venue.address"
+        :caption="venue.address"
+        :indicator="'true'"
+      />
+    </p>
+  </div>
 </template>
 
 <script>
@@ -23,5 +25,27 @@ import ExternalLink from '../Utils/ExternalLink.vue'
 
 export default {
   components: { ExternalLink },
+  computed: {
+    venues () {
+      let addresses = this.$page.frontmatter.address
+      let venues = this.$page.frontmatter.venue
+      // Multiple venues
+      if (Array.isArray(venues)) {
+        let count = -1
+        return venues.map(venue => {
+          count++
+          return {
+            name: venue,
+            address: addresses[count]
+          }
+        })
+      }
+      // Single venue
+      return [{
+        name: venues || null,
+        address: addresses
+      }]
+    }
+  }
 }
 </script>
