05f784fb6f93111a138f350214dbae85011e1269
Carlos E. Salazar <ce.salazar@gmail.com>
2018-07-30 03:57:05 +0200

    Add link to repo, and to edit pages

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -12,7 +12,23 @@ module.exports = {
         link: '/calendar/'
       },
     ],
-    logo: 'logo_berlinblockchainweek.png'
+    logo: 'logo_berlinblockchainweek.png',
+    // Assumes GitHub. Can also be a full GitLab url.
+    repo: 'cesalazar/berlinblockchainweek',
+    // Customising the header label
+    // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
+    // repoLabel: 'Contribute!',
+    // Optional options for generating "Edit this page" link
+    // if your docs are in a different repo from your main project:
+    // docsRepo: 'vuejs/vuepress',
+    // if your docs are not at the root of the repo:
+    // docsDir: 'docs',
+    // if your docs are in a specific branch (defaults to 'master'):
+    docsBranch: 'master',
+    // defaults to false, set to true to enable
+    editLinks: true,
+    // custom text for edit link. Defaults to "Edit this page"
+    // editLinkText: 'Help us improve this page!'
   },
   // TODO: fix path of assets (not loading)
   // chainWebpack: (config, isServer) => {
