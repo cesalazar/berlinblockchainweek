8d770fc538687cc8880ac552330fc2c9a2d905f8
Carlos E. Salazar <ce.salazar@gmail.com>
2018-08-06 13:35:44 +0200

    Add events filters

--- .vuepress/components/EventsList.vue
+++ .vuepress/components/EventsList.vue
@@ -15,6 +15,25 @@
       </a>
     </h1>
 
+    <div class="filters">
+      <span>
+        <label>
+          <input name="category" type="radio" value="All" checked
+            @click="filterEvents('All')"
+          />
+          <span>All</span>
+        </label>
+      </span>
+      <span v-for="category in categories">
+        <label>
+          <input name="category" type="radio" :value="category"
+            @click="filterEvents(category)"
+          />
+          <span>{{ category }}s</span>
+        </label>
+      </span>
+    </div>
+
     <div v-for="day in days" :key="day">
       <h3 class="date-sticky">
         <DateTime :date="day"/>
@@ -54,6 +73,7 @@ export default {
     firstDay: new Date('2018-09-05'),
     duration: 7,
     descending: false,
+    categories: [],
     events: [],
     days: [],
   }),
@@ -62,17 +82,40 @@ export default {
       let events = this.$site.pages.map(event => {
         let eventName = event.frontmatter.name
         if (eventName && eventName !== 'Sample Template') {
-          let category = event.frontmatter.category
-          event.frontmatter.category = capitalizeWord(category)
+          let category = capitalizeWord(event.frontmatter.category)
+          event.frontmatter.category = category
+          this.setFilter(category)
           event.day = setEventDay(event.frontmatter.date)
           return event
         }
       })
       // Remove nulls
-      events = events.filter((event) => event != null)
+      events = events.filter(event => event)
       // Sort by date
       events = events.sort((a, b) => a.day - b.day)
       this.events = events
+    },
+    setFilter (category) {
+      let categories = this.categories
+      if (categories.indexOf(category) === -1) {
+        categories.push(category)
+        categories.sort((a, b) => {
+          if(a < b) return -1
+          if(a > b) return 1
+          return 0
+        })
+      }
+    },
+    filterEvents (category) {
+      this.setEvents()
+      if (category !== 'All') {
+        this.events = this.events.filter(event => {
+          return event.frontmatter.category === category
+        })
+      }
+      this.days = []
+      this.setDays()
+      if (this.descending) this.days.reverse()
     },
     reverseDates () {
       this.descending = !this.descending
@@ -95,13 +138,9 @@ export default {
       })
       if (count === 0) this.days.splice(-1, 1)
     },
-    capitalizeWord (word) {
-      return capitalizeWord(word)
-    }
   },
   mounted () {
-    this.setEvents()
-    this.setDays()
+    this.filterEvents('All')
   }
 }
 
@@ -118,7 +157,7 @@ function addDays (date, days) {
 
 </script>
 
-<style lang="stylus">
+<style scoped lang="stylus">
 @require './../theme/styles/config.styl'
 
 .events--container
@@ -128,6 +167,8 @@ function addDays (date, days) {
     background #000
     .datetime
       padding-top 5px
+      opacity: 1
+      color: #fff
   .action-button
     cursor pointer
     display inline-block
@@ -140,25 +181,29 @@ function addDays (date, days) {
     box-sizing border-box
     float right
     margin-top 4px
+    &:hover
+      text-decoration: none !important
     span
       font-size 1.3em
   div
     margin 2em 0 4em
-    
-.action-button
-  &:hover
-    text-decoration: none !important
-    
-.datetime
-  color: rgba(255, 255, 255, 0.8)
-  
-h3 > .datetime
-  opacity: 1
-  color: #fff
-  
-h2
-  border-bottom: none
-  
-.events--container > div
-  border-bottom 1px solid rgba(255, 255, 255, .3)
+  &>div
+    border-bottom 1px solid rgba(255, 255, 255, .3)
+  .datetime
+    color: rgba(255, 255, 255, 0.8)
+  h2
+    border-bottom: none
+  .filters
+    margin-bottom 0
+    border-bottom 0 none
+    display: flex
+    justify-content space-between
+    flex-wrap wrap
+  label
+    margin-right 1em
+    cursor pointer
+    input
+      margin-top -3px
+      vertical-align middle
+      cursor pointer
 </style>
