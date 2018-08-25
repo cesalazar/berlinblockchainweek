b390f9823e7e96279260a3e0b4245f5ae50b9c3f
Carlos E. Salazar <ce.salazar@gmail.com>
2018-08-25 16:04:55 +0200

    Add sort filter to EventsList component

--- .vuepress/components/EventsList.vue
+++ .vuepress/components/EventsList.vue
@@ -28,6 +28,12 @@
           <span>{{ category }}s</span>
         </label>
       </div>
+      <div>
+        <span class="date--sort" @click="reverseDates()">
+          Sort
+          <svg :class="{ descending: descending }" enable-background="new 0 0 292.362 292.362" height="292.362" viewBox="0 0 292.362 292.362" width="292.362" xmlns="http://www.w3.org/2000/svg"><path d="m286.935 69.377c-3.614-3.617-7.898-5.424-12.848-5.424h-255.813c-4.952 0-9.233 1.807-12.85 5.424-3.617 3.621-5.424 7.902-5.424 12.851 0 4.948 1.807 9.229 5.424 12.847l127.907 127.907c3.621 3.617 7.902 5.428 12.85 5.428s9.233-1.811 12.847-5.428l127.907-127.908c3.613-3.617 5.427-7.898 5.427-12.847 0-4.948-1.814-9.229-5.427-12.85z"/></svg>
+        </span>
+      </div>
     </div>
 
     <div v-for="day in days" :key="day">
@@ -199,6 +205,15 @@ function addDays (date, days) {
     display flex
     justify-content space-between
     flex-wrap wrap
+    .date--sort
+      cursor pointer
+      svg
+        margin-left 0.2em
+        width 0.8em
+        height 0.8em
+        fill #fff
+        &.descending
+          transform rotate(180deg)
     label
       cursor pointer
     div
